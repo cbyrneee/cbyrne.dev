dcb87ebf6b75cd2c2b51ccffde2e8ef0f749e65f
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-09 13:08:00 +0100

    chore: Make the dark theme the default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { Navigation } from '../elements/base';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider defaultTheme="system" attribute="class">
+    <ThemeProvider defaultTheme="dark" attribute="class">
       <div className="container mx-auto max-w-4xl p-4 pt-8 lg:p-0 lg:py-12 flex flex-col gap-8">
         <Navigation />
         <Component {...pageProps} />
