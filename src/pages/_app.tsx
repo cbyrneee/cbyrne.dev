e34e2aa4100d2ef695a7a5116f9b7fd23fe4af19
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-09 12:00:23 +0100

    chore(app): Embed page in global container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,5 +4,9 @@ import type { AppProps } from 'next/app';
 import '@fontsource/inter/variable.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container mx-auto max-w-4xl p-4 pt-8 lg:p-0 lg:py-12">
+      <Component {...pageProps} />
+    </div>
+  );
 }
