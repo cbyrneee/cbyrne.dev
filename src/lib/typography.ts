81959abc09e3eb587341f843dbb7e94056878f7e
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-06-09 12:28:10 +0100

    chore: Add dark mode support with next-themes

--- src/lib/typography.ts
+++ src/lib/typography.ts
@@ -1,6 +1,6 @@
 const typography = {
-  title: 'text-3xl font-bold',
-  subtitle: 'text-xl font-medium text-neutral-600',
+  title: 'text-3xl font-bold dark:text-neutral-300',
+  subtitle: 'text-xl text-neutral-600 dark:text-neutral-400',
 };
 
 export default typography;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-
 import '@fontsource/inter/variable.css';
+
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto max-w-4xl p-4 pt-8 lg:p-0 lg:py-12">
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="container mx-auto max-w-4xl p-4 pt-8 lg:p-0 lg:py-12">
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   );
 }
